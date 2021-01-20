5e95869bc63504becd8ec1221146dfe17ce2b1b9
Tu Phan <tuantu.phan@cimb.com>
2021-01-20 15:35:24 +0800

    Add

---
+++ src/main/java/com/cweb/team/App.java
@@ -0,0 +1,19 @@
+package com.cweb.team;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class App {
+    public static void main(String[] args) {
+        String connectionUrl = "jdbc:sqlserver://10.112.224.218:1433;databaseName=testDB;user=CWEB;password=Pass1234!";
+        try {
+            // Load SQL Server JDBC driver and establish connection.
+            System.out.print("Connecting to SQL Server ... \n");
+            try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+                System.out.println("Done.");
+            }
+        } catch (Exception e) {
+            System.out.println("Can not connect");
+        }
+    }
+}
